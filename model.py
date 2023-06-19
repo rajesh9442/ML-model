ce2784269791a6069aec1e100a498a36324b9e4b
rajesh9442 <rajeshmahendran1999@gmail.com>
2023-06-19 00:05:31 -0400

    Built and trained the model

---
+++ model.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+
+# Load the training and test datasets
+train_df = pd.read_csv("C:/Users/rajes/Downloads/train.csv")
+test_df = pd.read_csv("C:/Users/rajes/Downloads/test.csv")
+
+x=train_df.drop(columns='Class')
+y=train_df.Class
+
+# Encode the 'Id' and 'EJ' columns in the training dataset
+label_encoder = LabelEncoder()
+train_df['Id'] = label_encoder.fit_transform(train_df['Id'])
+train_df['EJ'] = label_encoder.fit_transform(train_df['EJ'])
+
+test_df['Id'] = label_encoder.fit_transform(test_df['Id'])
+test_df['EJ'] = label_encoder.fit_transform(test_df['EJ'])
+
+X_train = train_df.drop('Class', axis=1)
+Y_train = train_df['Class']
+
+imputer = SimpleImputer(strategy='mean')
+X_train = imputer.fit_transform(X_train)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+
+model = LogisticRegression(solver='saga', max_iter=5000)
+model.fit(X_train_scaled, Y_train)
+
