882e416e01770f56b02c9d794309e8d321571c07
rajesh9442 <rajeshmahendran1999@gmail.com>
2023-06-21 13:14:42 -0400

    Implemented one-hot-encoding

--- model.py
+++ model.py
@@ -5,42 +5,33 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import make_pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
 
 # Load the training and test datasets
-train_df = pd.read_csv("C:/Users/rajes/Downloads/train.csv")
-test_df = pd.read_csv("C:/Users/rajes/Downloads/test.csv")
+train_df = pd.read_csv("/content/train.csv")
+test_df = pd.read_csv("/content/test.csv")
 
-x=train_df.drop(columns='Class')
-y=train_df.Class
+# Perform one hot encoding on training dataset
+train_df_new = pd.get_dummies(train_df, columns=['EJ'],  dtype=float).dropna()
 
-# Encode the 'Id' and 'EJ' columns in the training dataset
-label_encoder = LabelEncoder()
-train_df['Id'] = label_encoder.fit_transform(train_df['Id'])
-train_df['EJ'] = label_encoder.fit_transform(train_df['EJ'])
+# Split the training dataset into features (X) and target labels (Y)
+X_train = train_df_new.drop(['Id','Class'], axis=1)
+Y_train = train_df_new['Class']
 
-test_df['Id'] = label_encoder.fit_transform(test_df['Id'])
-test_df['EJ'] = label_encoder.fit_transform(test_df['EJ'])
-
-X_train = train_df.drop('Class', axis=1)
-Y_train = train_df['Class']
-
-imputer = SimpleImputer(strategy='mean')
-X_train = imputer.fit_transform(X_train)
-
-scaler = StandardScaler()
-X_train_scaled = scaler.fit_transform(X_train)
-
+#Training the model
 model = LogisticRegression(solver='saga', max_iter=5000)
-model.fit(X_train_scaled, Y_train)
+model.fit(X_train, Y_train)
 
-X_test = test_df.copy()
-X_test = imputer.transform(X_test)
-X_test_scaled = scaler.transform(X_test)
+# Perform onehot encoding for test data
+test_df.dtypes
+X_test = test_df.drop(['Id'],axis=1)
+X_test = pd.get_dummies(X_test, columns=['EJ'],  dtype=float, sparse=False)
 
-Y_test_pred = model.predict(X_test_scaled)
+#Predicting the output
+Y_test_pred = model.predict(X_test)
 
 print(Y_test_pred)
